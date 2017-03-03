b0dd16bc3a7e4f2497d37a48b98198c5babd8ddc
Jakub Kopański <jkopansk@gmail.com>
2017-03-03 21:40:49 +0100

    setup __DEV__ global

--- test/setup.js
+++ test/setup.js
@@ -7,6 +7,8 @@ global.document = jsdom('<!doctype html><html><body></body></html>')
 global.window = document.defaultView
 global.navigator = global.window.navigator
 
+global.__DEV__ = false
+
 // nasty localStorage hack
 global.window.localStorage = {
   getItem: function (key) {
